ab804b278ba2b01c17275f09ce663bd5fc2c09a1
Maxime Rochat <rochat.max@gmail.com>
2026-04-13 17:34:54 +0200

    Added extern symbol for ringbuffers

--- Application/Kalman/kalman_process.cpp
+++ Application/Kalman/kalman_process.cpp
@@ -9,6 +9,10 @@ extern "C" {
 }
 #include "Drivers/InvIMU/InvIMU.hpp"
 
+extern RingBuffer<IMUData, 100> imuData1;
+extern RingBuffer<IMUData, 100> imuData2;
+extern RingBuffer<IMUData, 100> imuData3;
+
 int kalman_loop() {
 	return 0;
 }
