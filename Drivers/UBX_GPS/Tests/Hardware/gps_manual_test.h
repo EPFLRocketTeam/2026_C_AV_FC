63aabc8384851ae71aebd5db9946e355f0305d7a
Maxime Rochat <rochat.max@gmail.com>
2026-04-30 12:04:48 +0200

    added UBX driver

---
+++ Drivers/UBX_GPS/Tests/Hardware/gps_manual_test.h
@@ -0,0 +1,23 @@
+/*
+ * gps_manual_test.h
+ *
+ *  Created on: Feb 15, 2026
+ *      Author: maxime
+ */
+
+#ifndef UBX_GPS_TESTS_HARDWARE_GPS_MANUAL_TEST_H_
+#define UBX_GPS_TESTS_HARDWARE_GPS_MANUAL_TEST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int manual_test_gps();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* UBX_GPS_TESTS_HARDWARE_GPS_MANUAL_TEST_H_ */
